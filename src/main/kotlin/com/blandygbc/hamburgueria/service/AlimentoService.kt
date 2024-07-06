bd5defa78c2db75b273d0f027fb711267f8679e8
Gabriel Blandy <gabriel_blandy@hotmail.com>
2024-07-06 18:21:51 -0300

    Add annotation transactional and hibernate show sql

--- src/main/kotlin/com/blandygbc/hamburgueria/service/AlimentoService.kt
+++ src/main/kotlin/com/blandygbc/hamburgueria/service/AlimentoService.kt
@@ -6,17 +6,22 @@ import com.blandygbc.hamburgueria.domain.alimento.AlimentoUpdate
 import com.blandygbc.hamburgueria.exception.ElementoNaoEncontradoException
 import com.blandygbc.hamburgueria.repository.AlimentoRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AlimentoService(private val alimentoRepository: AlimentoRepository) {
+
+    @Transactional
     fun salvar(alimentoNovo: AlimentoNovo): Alimento {
         return alimentoRepository.save(alimentoNovo.paraDominio())
     }
 
+    @Transactional(readOnly = true)
     fun buscarTodos(): List<Alimento> {
         return alimentoRepository.findAll()
     }
 
+    @Transactional(readOnly = true)
     fun buscarAlimento(pesquisa: String): Alimento {
         val id = pesquisa.toLongOrNull()
             ?: return alimentoRepository
@@ -32,10 +37,12 @@ class AlimentoService(private val alimentoRepository: AlimentoRepository) {
             }
     }
 
+    @Transactional
     fun removerAlimento(id: Long) {
         alimentoRepository.deleteById(id)
     }
 
+    @Transactional
     fun atualizarAlimento(idAlimento: Long, alimentoReq: AlimentoUpdate): Alimento? {
         val alimento = alimentoRepository
             .findById(idAlimento)
